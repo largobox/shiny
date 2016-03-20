14c03822361443d661aa1f7ad5a4d7eaf6dad242
Eventman <sergey.event@gmail.com>
2016-03-20 18:12:28 +0400

    Fix for artile show page

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ActiveRecord::Base
   has_many :shiny_comments
   has_and_belongs_to_many :heros
 
-  has_attached_file :article_pic, styles: { normal: '320x198>' }, default_url: 'dota_default.jpg'
+  has_attached_file :article_pic, styles: { normal: '640x396>' }, default_url: 'dota_default.jpg'
   validates_attachment_content_type :article_pic, content_type: /\Aimage\/.*\Z/
   
 end
